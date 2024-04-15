9a6c0edca701c08cdc26ad27e347e6b1cf4e00aa
Sony Chen <hello@sony-chen.com>
2024-04-14 21:54:31 -0600

    Fixed functions

---
+++ scripts/build-shows-page-details.js
@@ -0,0 +1,27 @@
+const showDetails = [
+    {
+        date: "Tue Sept 17 2024",
+        venue: "Pier 3 East",
+        location: "San Francisco, CA",
+    },
+    {
+        date: "Sat Oct 12 2024",
+        venue: "View Lounge",
+        location: "San Francisco, CA",
+    },
+    {
+        date: "Sat Nov 16 2024",
+        venue: "Hyatt Agency",
+        location: "San Francisco, CA",
+    },
+    {
+        date: "Fri Nov 29 2024",
+        venue: "Moscow Center",
+        location: "San Francisco, CA",
+    },
+    {
+        date: "Wed Dec 18 2024",
+        venue: "Press Club",
+        location: "San Francisco, CA",
+    },
+    ];
--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,31 +1,3 @@
-const showDetails = [
-{
-    date: "Tue Sept 17 2024",
-    venue: "Pier 3 East",
-    location: "San Francisco, CA",
-},
-{
-    date: "Sat Oct 12 2024",
-    venue: "View Lounge",
-    location: "San Francisco, CA",
-},
-{
-    date: "Sat Nov 16 2024",
-    venue: "Hyatt Agency",
-    location: "San Francisco, CA",
-},
-{
-    date: "Fri Nov 29 2024",
-    venue: "Moscow Center",
-    location: "San Francisco, CA",
-},
-{
-    date: "Wed Dec 18 2024",
-    venue: "Press Club",
-    location: "San Francisco, CA",
-},
-];
-
 const showListDiv = document.querySelector(".shows__table");
 
 for (let i = 0; i < showDetails.length; i++){
